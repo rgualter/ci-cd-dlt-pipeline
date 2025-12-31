02db61412b64d7755a8eab21c60f6c237052e5bc
Ricardo Gualter <ricardo.gualter@gmail.com>
2025-12-31 13:41:48 -0300

    update in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project",
-    version="0.0.1",
+    version="0.0.2",
     description="Databricks project for Citibike ETL pipeline",
     author="Ricardo Gualter",
     packages=find_packages(where="./src"),
